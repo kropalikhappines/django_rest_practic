a3cba7eccb76cd7372c2c8507d2b2883c7110f6e
kropalikhappines <arsik30394@gmail.com>
2022-03-30 14:10:29 +0300

    Added pagination to the projects class limit 2

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,14 +1,19 @@
+from email.policy import default
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Projects, ToDo
 from .serializers import ProjectModelSerializers, ToDoModelSerializers
 # Create your views here.
 
+
+class ProjectsLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 2
 class ProjectsModelViewSet(ModelViewSet):
     queryset = Projects.objects.all()
     serializer_class = ProjectModelSerializers
+    pagination_class = ProjectsLimitOffsetPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
