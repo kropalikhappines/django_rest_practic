2aea9225c2f01a10ad11e163f3bb03fbfe2d93dc
kropalikhappines <arsik30394@gmail.com>
2022-04-07 13:47:45 +0300

    add groups

--- todo/settings.py
+++ todo/settings.py
@@ -144,6 +144,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
     'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
