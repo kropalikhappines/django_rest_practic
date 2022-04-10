143bd1aa107638f44c3c93d5823aab260e1ae28b
kropalikhappines <arsik30394@gmail.com>
2022-04-10 16:37:01 +0300

    add tokens frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import ProjectList from './components/Projects';
 import TodoList from './components/todo';
 import NotFound404 from './components/NotFound';
 import ProjItemList from './components/ProjectDeateil';
+import LoginForm from './components/Auth';
 import {HashRouter, Route, BrowserRouter, Link, Switch, Redirect} from 'react-router-dom'
 
 
@@ -19,30 +20,46 @@ class App extends React.Component {
       users : [],
       projects : [],
       todos : [],
+      token: '',
     }
   }
 
+  load_data() {
+  axios.get('http://127.0.0.1:8000/api/users/')
+    .then(response => {
+      // console.log(response.data.results)
+        this.setState({users: response.data.results})
+      }).catch(error => console.log(error))
+  
+  axios.get('http://127.0.0.1:8000/api/Projects/')
+    .then(response => {
+      // console.log(response.data.results)
+        this.setState({projects: response.data.results})
+      }).catch(error => console.log(error))
+
+  axios.get('http://127.0.0.1:8000/api/ToDo/')
+    .then(response => {
+      // console.log(response.data.results)
+        this.setState({todos: response.data.results})
+      }).catch(error => console.log(error))
+
+  }
+
+
+  get_token(username, password){
+    
+    axios.post('http://127.0.0.1:8000/api/token/', {'username': username, password: password})
+    .then(response => {
+      // console.log(response.data.results)
+      console.log(response.data['access'])
+    }).catch(error => console.log(error))  }
+
 
   componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/users/')
-      .then(response => {
-        // console.log(response.data.results)
-          this.setState({users: response.data.results})
-        }).catch(error => console.log(error))
-      
-    axios.get('http://127.0.0.1:8000/api/Projects/')
-      .then(response => {
-        // console.log(response.data.results)
-          this.setState({projects: response.data.results})
-        }).catch(error => console.log(error))
-
-    axios.get('http://127.0.0.1:8000/api/ToDo/')
-      .then(response => {
-        // console.log(response.data.results)
-          this.setState({todos: response.data.results})
-        }).catch(error => console.log(error))
-    
+    this.load_data()
   }
+
+
   render () {
     return (
       <div>
@@ -64,8 +81,12 @@ class App extends React.Component {
             <Route exact path='/' component={() => <UserList users={this.state.users} />}/>
             <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects} />}/>
             <Route exact path='/todo' component={() => <TodoList todos={this.state.todos} />}/>
+            
+            
             <Route exact path='/project/:id' component={() => <ProjItemList projects={this.state.projects} users={this.state.users} />}/>
-{/* 
+
+            <Route exact path='/login' component={() => <LoginForm get_token={(username,password)=> this.get_token(username,password)} />}/>
+{/*         
             <Route path='/projects/:id'>
               <ProjItemList projects={this.state.projects}/>
             </Route> */}
